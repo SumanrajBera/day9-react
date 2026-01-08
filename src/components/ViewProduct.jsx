033755d90a702f21a489d284347a03a4827a0a04
SumanrajBera <srbera17@gmail.com>
2026-01-08 21:27:25 +0530

    Product view feature added

---
+++ src/components/ViewProduct.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ViewProduct = ({product, rating}) => {
+    return (
+        <div className='productDets'>
+            <div className="image">
+                <img src={product.image} alt="" />
+            </div>
+            <div className="details">
+                <h1>{product.title}</h1>
+                <p>{product.description}</p>
+                <p className='price'><strong>Price:</strong> <span>Rs.{product.price}</span></p>
+                <div className="rating">
+                    <p><strong>Rating:</strong> <span  className="stars">{new Array(Math.floor(rating)).fill(<i class="ri-star-fill"></i>)}</span></p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewProduct
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AllProductsContext } from '../context/AllProducts';
+import ViewProduct from '../components/ViewProduct';
 
 const Product = () => {
+  let allProducts = useContext(AllProductsContext)
+  let { id } = useParams();
+  const [product, setProduct] = useState({})
+  const [rating, setRating] = useState(0)
+
+  useEffect(() => {
+    if (!allProducts.length) return;
+    const currentProduct = allProducts.find(
+      el => String(el.id) === String(id)
+    );
+    console.log(currentProduct)
+
+    setProduct(currentProduct)
+    setRating(currentProduct.rating.rate)
+
+  }, [id, allProducts])
+
   return (
-    <div>Product</div>
+    <ViewProduct product={product} rating={rating}/>
   )
 }
 
